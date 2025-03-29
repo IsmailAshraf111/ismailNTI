7e4cccd6f760fd2cfe000609ede62807d1cd2aa9
Ismail Ashraf <154062194+IsmailAshraf111@users.noreply.github.com>
2025-03-30 01:49:32 +0200

    save updates before update angular

--- Mean-Stack-NTI-Graduation-Project/backEnd-Graduation-Project/routes/contact-me-route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const contactController = require("../controllers/contact-me-controllers");
-
-router.get("/", contactController.getContact);
-router.post("/", contactController.createContact);
-router.put("/:id", contactController.updateContact);
-
-module.exports = router;
